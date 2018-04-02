36cef24863c88ea97156b2616b785dc93d493c4f
pranavd <pranavd@sfu.ca>
2018-04-02 14:23:59 -0700

    add function: getMovieDetailsFromJSON to JSONUtils to get other data required for the detail activity

--- app/src/main/java/com/example/android/popularmovies/Utilities/JSONUtils.java
+++ app/src/main/java/com/example/android/popularmovies/Utilities/JSONUtils.java
@@ -33,4 +33,35 @@ public class JSONUtils {
         return movieImages;
 
     }
+
+    public static String[][] getMovieDetailsFromJSON(Context context, String movieJsonString) throws JSONException {
+
+        String[][] movieDetails = null;
+
+        JSONObject movieJson = new JSONObject(movieJsonString);
+        JSONArray movieArray = movieJson.getJSONArray("results");
+        movieDetails = new String[movieArray.length()][5];
+
+        for(int i =0 ; i< movieArray.length();i++){
+
+            JSONObject movie = movieArray.getJSONObject(i);
+
+            String[] movieDetail = new String[5];
+            movieDetail[0] = movie.getString("title");
+
+            String image = movie.getString("poster_path");
+            image = image.split("/")[1];
+            String imagePath = NetworkUtils.buildImagePath(image);
+            movieDetail[1] = imagePath;
+
+            movieDetail[2] = movie.getString("overview");
+            movieDetail[3] = String.valueOf(movie.getLong("vote_average"));
+            movieDetail[4] = movie.getString("release_date");
+
+            movieDetails[i] = movieDetail;
+
+        }
+        return movieDetails;
+
+    }
 }
