f271dd80ad837a1e06655b918e97091c3f641da1
pranavd <pranavd@sfu.ca>
2018-04-02 11:17:57 -0700

    add member variables for recyclerview and recyclerview adapter and conect them both, set the layout manager for recyclerview as a gridlayout manager

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecylerView;
+        MovieAdapter mMovieAdapter;
+
+        mRecylerView = (RecyclerView) findViewById(R.id.movies_recyclerview);
+        mMovieAdapter = new MovieAdapter();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(MainActivity.this,2);
+
+        mRecylerView.setLayoutManager(layoutManager);
+
+        mRecylerView.setAdapter(mMovieAdapter);
+
     }
 }
