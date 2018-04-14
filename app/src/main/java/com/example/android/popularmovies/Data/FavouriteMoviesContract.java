b5edab17738f94b79d8395a8861e523000277c9a
pranavd <pranavd@sfu.ca>
2018-04-14 03:15:22 -0700

    create a package for handling database and content provider, create a contract for the database

---
+++ app/src/main/java/com/example/android/popularmovies/Data/FavouriteMoviesContract.java
@@ -0,0 +1,26 @@
+package com.example.android.popularmovies.Data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class FavouriteMoviesContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.popularmovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_FAVOURITES = "favouriteMovies";
+
+    public static final class MovieItem implements BaseColumns{
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVOURITES).build();
+
+        public static final String TABLE_NAME ="favouriteMovies";
+
+        public static final String MOVIE_TITLE = "title";
+        public static final String MOVIE_POSTER = "poster";
+        public static final String MOVIE_RATING = "rating";
+        public static final String MOVIE_RELEASE_DATE = "release_date";
+        public static final String MOVIE_SYNOPSIS = "overview";
+
+
+    }
+}
